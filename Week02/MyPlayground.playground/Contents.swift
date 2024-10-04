0d70ee32eb3542c357e93a6d9fe9aca601e5f748
JanaeSylOli <147261259+JanaeSylOli@users.noreply.github.com>
2024-10-03 22:00:23 -0400

    kitten

--- Week02/MyPlayground.playground/Contents.swift
+++ Week02/MyPlayground.playground/Contents.swift
@@ -1,3 +1,23 @@
-import UIKit
+//Janae Sylvester Oliver
+// 10.02.2024
+// Ascii kitten
 
-var greeting = "Hello, playground"
+
+import UIKit
+import PlaygroundSupport
+
+
+let kittenAscii = """
+     /\\_/\\  
+    ( o.o ) 
+     > ^ <
+"""
+
+let label = UILabel()
+label.font = UIFont(name: "Courier", size: 32)
+label.text = kittenAscii
+label.numberOfLines = 0
+label.sizeToFit()
+
+
+PlaygroundPage.current.liveView = label
