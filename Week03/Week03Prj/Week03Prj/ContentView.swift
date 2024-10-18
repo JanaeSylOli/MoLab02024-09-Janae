4f3923f6cbe6038668f88dd83d67daebe034d4fa
JanaeSylOli <147261259+JanaeSylOli@users.noreply.github.com>
2024-10-18 11:35:10 -0400

    Updated

---
+++ Week03/Week03Prj/Week03Prj/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Week03Prj
+//
+//  Created by Janae  Sylvester Oliver on 10/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Week03/Week03Prj/Week03Prj/Week03PrjApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week03PrjApp.swift
+//  Week03Prj
+//
+//  Created by Janae  Sylvester Oliver on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Week03PrjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
