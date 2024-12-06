41c1522da20ff396379925eb071350fc6153e0b3
JanaeSylOli <147261259+JanaeSylOli@users.noreply.github.com>
2024-12-05 21:46:50 -0500

    update

---
+++ BDayPlanner/BDayPlanner/BDayPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BDayPlannerApp.swift
+//  BDayPlanner
+//
+//  Created by Janae  Sylvester Oliver on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct BDayPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
